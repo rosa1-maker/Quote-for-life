066d808211bf821dc166a39b8c409f2dc8f83402
rosa1-maker <teresawanjiku2000@gmail.com>
2020-01-13 04:54:44 +0300

    added some quotes in quotes.component.ts

--- src/app/qoutes/qoutes.component.ts
+++ src/app/qoutes/qoutes.component.ts
@@ -1,34 +1,34 @@
-// import { Component, OnInit } from '@angular/core';
-// import{ Qoutes} from '../qoutes'
-// @Component({
-//   selector: 'app-qoutes',
-//   templateUrl: './qoutes.component.html',
-//   styleUrls: ['./qoutes.component.css']
-// })
-// export class QoutesComponent implements OnInit {
+import { Component, OnInit } from '@angular/core';
+import{ Qoutes} from '../qoutes'
+@Component({
+  selector: 'app-qoutes',
+  templateUrl: './qoutes.component.html',
+  styleUrls: ['./qoutes.component.css']
+})
+export class QoutesComponent implements OnInit {
 
 
-//   quotes=[
-//     new Qoutes(1, 'To Love is to care.', 'tressy', "Teresa Clerk"),
-//     new Qoutes(2,'When you cannot find the sunshine be the sunshine', 'Bliss', "Maccnon Law"),
-//     new Qoutes(3, 'The grass is greener when you water it', 'Liz', "Faking Hay")
-//   ];
+  quotes=[
+    new Qoutes(1, 'To Love is to care.', 'tressy', "Teresa Clerk"),
+    new Qoutes(2,'When you cannot find the sunshine be the sunshine', 'Bliss', "Maccnon Law"),
+    new Qoutes(3, 'The grass is greener when you water it', 'Liz', "Faking Hay")
+  ];
 
-//   addNewQoutes(quotes){
-//     let qoutesLength=this.quotes,length;
-//     quotes.id=qoutesLength;
-//     this.quotes.unshift(quotes)
+  addNewQoutes(quotes){
+    let qoutesLength=this.quotes,length;
+    quotes.id=qoutesLength;
+    this.quotes.unshift(quotes)
 
-//   }
+  }
 
-//   toggleDetails(index){
-//     this.quotes[index].showAuthor =!this.quotes[index].showAuthor;
-//    }
+  toggleDetails(index){
+    this.quotes[index].showAuthor =!this.quotes[index].showAuthor;
+   }
 
 
-//   constructor() { }
+  constructor() { }
 
-//   ngOnInit() {
-//   }
+  ngOnInit() {
+  }
 
-// }
+}
