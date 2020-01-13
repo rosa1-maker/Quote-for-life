cb7fbc277dd5f5a4fca72b65a7097d64b06474aa
rosa1-maker <teresawanjiku2000@gmail.com>
2020-01-13 16:42:55 +0300

    added an image in assets to act as backgriund image

--- src/app/qoute-details/qoute-details.component.ts
+++ src/app/qoute-details/qoute-details.component.ts
@@ -1,36 +1,32 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { Qoutes } from '../qoutes'
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { Qoutes } from "../qoutes";
 @Component({
-  selector: 'app-qoute-details',
-  templateUrl: './qoute-details.component.html',
-  styleUrls: ['./qoute-details.component.css']
+  selector: "app-qoute-details",
+  templateUrl: "./qoute-details.component.html",
+  styleUrls: ["./qoute-details.component.css"]
 })
 export class QouteDetailsComponent implements OnInit {
-
   votes: number;
 
-  @Input () qoutes:Qoutes;
+  @Input() qoutes: Qoutes;
   vote = 0;
   @Output() isComplete = new EventEmitter<boolean>();
 
-  qouteDelete( complete:boolean){
+  qouteDelete(complete: boolean) {
     this.isComplete.emit(complete);
   }
-  numberoflikes : number=0
-  numberofdislikes : number=0
-     likebuttonclick(){
-       this.numberoflikes++;
-     }
-     dislikebutttonclick(){
-       this.numberofdislikes++;
-     }
+  numberoflikes: number = 0;
+  likebuttonclick() {
+    this.numberoflikes++;
+  }
+  numberofdislikes: number = 0;
+  dislikebutttonclick() {
+    this.numberofdislikes++;
+  }
 
   constructor() {
     this.votes = 0;
-   }
-
-    ngOnInit() {
-
   }
 
+  ngOnInit() {}
 }
